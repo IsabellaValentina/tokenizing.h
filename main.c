2b53f5121b902f7d49c80035a35bd1bb73605a62
IsabellaValentina <84825328+IsabellaValentina@users.noreply.github.com>
2021-08-13 16:47:57 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+ #include "fundamentals.h"
+#include "manipulating.h"
+#include "converting.h"
+#include "Tokenizing.h"
+
+
+int main() {
+
+ 	fundamentals();
+	manipulating();
+	converting();
+	tokenizing();
+	return 0;
+}
